60f4fdd1bd0127ea1f14193eefa8c2bce23623eb
Tsuisuto369 <riegertthomas1@gmail.com>
2026-05-27 03:34:48 +0200

    Statut fixe

--- src/events/ready.js
+++ src/events/ready.js
@@ -6,20 +6,6 @@ module.exports = {
   execute(client) {
     console.log(`\n🌟 369 Bot est en ligne ! Connecté en tant que ${client.user.tag}`);
     console.log(`📊 Présent sur ${client.guilds.cache.size} serveur(s)\n`);
-
-    const activities = [
-      { name: '/aide pour mes commandes', type: ActivityType.Playing },
-      { name: 'vos messages ✨', type: ActivityType.Watching },
-      { name: 'la musique des serveurs', type: ActivityType.Listening },
-    ];
-
-    let i = 0;
-    const setActivity = () => {
-      client.user.setActivity(activities[i % activities.length]);
-      i++;
-    };
-
-    setActivity();
-    setInterval(setActivity, 15_000);
+    client.user.setActivity('/aide pour mes commandes', { type: ActivityType.Playing });
   },
 };
